07d7c982b40c58a39e4106295912debd9e193f42
jrichir <jrichir@student.s19.be>
2025-02-24 11:39:48 +0100

    split error msgs on two lines to meet subject requirements

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 21:50:31 by ngharian          #+#    #+#             */
-/*   Updated: 2025/02/24 11:09:25 by jrichir          ###   ########.fr       */
+/*   Updated: 2025/02/24 11:38:41 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_exit_error(char *message)
 {
-	write(2, "Error!: ", 8);
+	write(2, "Error\n", 6);
 	if (message != NULL)
 		write(2, message, ft_strlen(message));
 	exit (1);
