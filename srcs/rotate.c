1f59fce0e28a906782f55a2e66cc7d0b45859bc7
noahghariani <noah.ghariani@gmail.com>
2025-03-30 12:11:28 +0200

    c good normalement

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -6,7 +6,7 @@
 /*   By: ngharian <ngharian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 18:05:13 by ngharian          #+#    #+#             */
-/*   Updated: 2025/03/29 18:41:50 by ngharian         ###   ########.fr       */
+/*   Updated: 2025/03/30 12:10:44 by ngharian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,25 +51,28 @@ void	rotate_r(t_info *infos)
 
 int	mouse_rotate(t_info *infos, int x)
 {
-	double old_dir_x = infos->dir_x;
-	double old_plane_x = infos->plane_x;
-	double angle = x * infos->frame_time * 0.50;
+	double	old_dir_x;
+	double	old_plane_x;
+	double	angle;
+
+	old_dir_x = infos->dir_x;
+	old_plane_x = infos->plane_x;
+	angle = (x / infos->frame_time) * 0.00055;
 	infos->dir_x = infos->dir_x * cos(angle) - infos->dir_y * sin(angle);
 	infos->dir_y = old_dir_x * sin(angle) + infos->dir_y * cos(angle);
 	infos->plane_x = infos->plane_x * cos(angle) - infos->plane_y * sin(angle);
 	infos->plane_y = old_plane_x * sin(angle) + infos->plane_y * cos(angle);
-	return(0);
+	return (0);
 }
 
 int	mouse_moved(int x, int y, t_info *infos)
 {
-	
-	int delta_x;
+	int	delta_x;
 
-    delta_x = x - infos->moves.prev_mouse_x; // Calcul du déplacement relatif
-    infos->moves.mouse_rotation = -delta_x; // Stocker le déplacement relatif
-    infos->moves.mouse_move = 1; // Indiquer qu'un mouvement a eu lieu
-    infos->moves.prev_mouse_x = x; // Mettre à jour la position précédente
-    ++y; 
-    return (0);
+	delta_x = x - infos->moves.prev_mouse_x;
+	infos->moves.mouse_rotation = -delta_x;
+	infos->moves.mouse_move = 1;
+	infos->moves.prev_mouse_x = x;
+	++y;
+	return (0);
 }
